172692f7112aab9c56a81c016f1bf01d9e13bd22
cuong <cuongbatbai2001@gmail.com>
2023-05-02 20:40:38 +0700

    fix loading

--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -6,7 +6,7 @@ export default function Loading() {
       <div className=" w-full flex justify-center items-center flex-col h-screen">
         <div className="border-t-transparent border-solid animate-spin  rounded-full border-gray-400 border-8 h-16 w-16"></div>
         <div className="flex">
-          <span className="text-xl font-fontcuong font-medium mt-2">Server đang được khởi động</span>
+          <span className="text-xl font-fontcuong font-medium mt-2">Server Nodejs đang được khởi động</span>
           <div className="bg-blue-600  w-2 h-2 rounded-full mt-6 ml-1 animate-bounce blue-circle"></div>
           <div className="bg-green-600  w-2 h-2 rounded-full mt-6 ml-1 animate-bounce green-circle"></div>
           <div className="bg-red-600  w-2 h-2 rounded-full mt-6 ml-1 animate-bounce red-circle"></div>
